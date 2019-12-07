9f51c1dc27dec5be3f2e36b679a0b605e02e148e
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-12-06 19:19:06 -0700

    woops

--- src/components/CipherBinWrite/CipherBinWrite.js
+++ src/components/CipherBinWrite/CipherBinWrite.js
@@ -55,7 +55,7 @@ class CipherBinWrite extends Component {
 
   emulateProcessing = async () => {
     this.setState({ isLoading: true });
-    await this.sleep(1000);
+    await this.sleep(850);
     this.setState({
       isLoading: false,
       message: '',
