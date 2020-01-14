63bed06f8bc0d72bbc56aa554ff4ef63773f89c1
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-01-13 20:53:07 -0700

    buncha content changes

--- src/components/FAQs/FAQs.js
+++ src/components/FAQs/FAQs.js
@@ -12,7 +12,7 @@ const FAQs = () => (
         How secure is cipherbin?
       </h5>
       <p className="faq-answer section-indent-twenty">
-        cipherbin uses only HTTPS, meaning any data transferred to or from the server
+        cipherbin uses HTTPS only, meaning any data transferred to or from the server
         is encrypted. All messages are encrypted using 256-bit AES encryption before
         being stored on the server and the key to decrypt it is contained in the URL.
         This URL is not stored on the server, so only the link can decrypt the messages.
@@ -26,17 +26,17 @@ const FAQs = () => (
         The messages are encrypted using 256-bit AES encryption and put into a database.
       </p>
       <h5 className="faq-question">
-        Can I retrieve a note after it has been read?
+        Can I retrieve a message after it has been read?
       </h5>
       <p className="faq-answer section-indent-twenty">
-        No. It is removed from the system after being displayed once.
+        No. It is removed from the system entirely after being displayed once.
       </p>
       <h5 className="faq-question">
-        Is it possible to see a recently read note using browser history, the back button,
+        Is it possible to see a recently read message using browser history, the back button,
         or the recently closed tabs feature?
       </h5>
       <p className="faq-answer section-indent-twenty">
-        No. The note self-destructs after being read; there is no way to re-read it once it
+        No. The message self-destructs after being read; there is no way to re-read it once it
         has been read.
       </p>
       <h5 className="faq-question">
@@ -44,35 +44,26 @@ const FAQs = () => (
       </h5>
       <p className="faq-answer section-indent-twenty">
         No. The messages are encrypted using a key that is never stored on the server.
-        Only the valid URL can display the messages. Once the messages are viewed the
-        encrypted messages are removed from the system and the link can not be viewed
+        Only the valid URL can display a message. Once a message is viewed, the
+        encrypted message is removed from the database and the link cannot be viewed
         again.
       </p>
       <h5 className="faq-question">
-        How do I know the note has been read?
+        How do I know the message has been read?
       </h5>
       <p className="faq-answer section-indent-twenty">
         You can opt in to receive a read receipt email. Click on
         <code>Display additional options?</code> to enter an email address
-        when creating a message to receive a email notifying you the note has been
-        read. You can also enter a reference name to keep track of the note if
-        sending multiple URL&apos;s. The email address is encrypted in the
-        same fashion as messages and deleted upon the note being viewed.
+        when creating a message to receive a email notifying you the message has been
+        read. You can also enter a reference name to keep track of the message if
+        sending multiple URL&apos;s. The notification email address attached to
+        the message is also deleted upon the message being viewed.
       </p>
       <h5 className="faq-question">
-        But this is what public/private key (asymmetric) cryptography is for? Why not use PGP?
+        How long are unread messages stored on your servers?
       </h5>
       <p className="faq-answer section-indent-twenty">
-        Agreed. If you and your recipient can use asymmetric cryptography (like PGP)
-        then definitely do so. But most people using this service do not want to deal
-        with that! This is easy enough for anyone to use without the knowledge of asymmetric
-        cryptography.
-      </p>
-      <h5 className="faq-question">
-        How much time are unread notes stored on your servers?
-      </h5>
-      <p className="faq-answer section-indent-twenty">
-        After 30 days all unread notes are automatically deleted.
+        After 30 days any unread message is automatically deleted.
       </p>
     </div>
   </Container>
--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import './HowItWorks.css';
 
@@ -7,32 +8,35 @@ const HowItWorks = () => (
     <div className="how-it-works-wrapper">
       <h4>How it works</h4>
       <p className="section-indent-twenty">
-        cipherb.in is a free and open source message encryption service that allows you
-        to create encrypted messages that can only be read once before they&#39;re
-        destroyed. No one other than you and the recipient can read the message. This
-        includes cipherb.in!
+        cipherbin is a free and open source message encryption service that allows you
+        to share encrypted messages safely via a link. As soon as your message is opened,
+        it is immediately destoyed and the link is void. Note: Messages are encrypted before
+        being sent to our servers, so even cipherbin can&#39;t see them.
       </p>
+      <hr />
+      <h4>How to use it</h4>
       <p className="section-indent-twenty">
-        Type in your message and click &quot;encrypt&quot;. We provide you with a one time use
-        link for reading the message. As soon as anyone opens the link and clicks &quot;Read
-        Now&quot;, it is destroyed.
+        Type in your message and click &quot;encrypt&quot;. A one time use
+        link is generated for reading the message. As soon as anyone opens
+        the link to read it, it is destroyed. For instructions around using
+        the CLI, jump over to the <Link to="/cli">CLI Docs</Link>
       </p>
       <hr />
       <h4>Under the hood</h4>
       <p className="section-indent-twenty">
         The message is encrypted with Javascript in your browser using 256 bit AES
-        encryption. AES allows us to use a passphrase for encrypting content. cipherb.in
-        generates a random 10 character string to use as the passphrase (you also
-        have the option to provide your own passphrase).
+        encryption. AES allows us to use a passphrase for encrypting content. cipherbin
+        generates a random 10 character string to use as the passphrase.
       </p>
       <p className="section-indent-twenty">
-        The encrypted message is then sent to our servers with a randomly generated v4
-        uuid. This means even cipherb.in servers can not read your message. The
+        The encrypted message is then sent to our servers (even cipherbin cannot read
+        your message) with a randomly generated v4 uuid. This is intended to drastically
+        reduce the chance of ending up at that URL if you have not been given the link. The
         generated link has a query param with the uuid and the passphrase for decrypting
-        the message. Again, our servers do not see that passphrase at any point.
+        the message. Our servers also do not see that passphrase at any point.
       </p>
       <p className="section-indent-twenty">
-        When someone visits the link cipherb.in makes a request for the encrypted
+        When someone visits the link cipherbin makes a request for the encrypted
         message using it&#39;s uuid. Our servers return the message and immediately
         destroy it. When your browser receives the message, it uses the passphrase
         from the URL to decrypt the message, rendering it in plain text. If you
